79ab784911c4c843d8e7607452687bf4ed513d1d
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-22 15:12:36 -0400

    Created add and list for basic todo app

--- todo/todo.js
+++ todo/todo.js
@@ -3,6 +3,30 @@ var readline = require("readline");
 	//Creating readline to interface with command line
 	rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.setPrompt("$$ ");
+rl.setPrompt("-> ");
 rl.prompt();
 
+var list = [];
+
+var commands = {
+	ls:function() {
+		console.log(list);	
+	},
+	add:function(item){
+		list.push(item);	
+	}
+};
+
+//When user enters line, do function
+rl.on('line', function(line) {
+	//Split line by ' '
+	var input = line.split(' ');
+	//Take first input as command
+	var command = input.shift();
+	//Join input into string again
+	var parseInput = input.join(' ');
+
+	commands[command](parseInput);
+
+	rl.prompt();
+});
