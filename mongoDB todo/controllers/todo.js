ae61996dc966422805538847cb7e747190bc02a7
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-23 02:07:42 -0400

    Completed to-do application and removed node_modules

--- mongoDB todo/controllers/todo.js
+++ mongoDB todo/controllers/todo.js
@@ -30,7 +30,7 @@ controller.create = [
 
 controller.update = [
 	function(req,res,next) {
-		todo.findById(req.param('todoID'), function(err,todo){
+		todo.findById(req.param('todoID'), function(err,todo) {
 			if(err) return next(err);
 			if(!todo) return res.send(404);
 			req.todo = todo; //Passing info to next() using request
@@ -49,7 +49,17 @@ controller.update = [
 
 controller.delete = [
 	function(req,res,next){
-
+		if("name" in req.body && req.body.name !== ''){
+				next();
+		} else {
+			res.send(400);
+		}
+	},
+	function(req,res,next) {
+		todo.remove(req.body, function(err,todo){
+			if(err) return next(err);
+			res.redirect("/");
+		});
 	}
 ];
 
--- mongoDB todo/server.js
+++ mongoDB todo/server.js
@@ -11,13 +11,13 @@ var	todoController = require('./controllers/todo');
 mongoose.connect('mongodb://localhost/mongoToDo')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine','ejs');
 
 app.get('/',todoController.index);
 app.post('/',todoController.create);
 app.put('/:todoID',todoController.update);
-app.del('/:todoID',todoController.delete);
+app.post('/delete',todoController.delete);
 
 app.listen(port,function(err) {
 	console.log('listening on %s',port);
